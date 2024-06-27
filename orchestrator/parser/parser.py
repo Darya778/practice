2fb5809702c17ee8c9890b116951eb5e016d8128
wotiwan <146071322+wotiwan@users.noreply.github.com>
2024-06-28 01:53:38 +0800

    added daemons list parser

---
+++ orchestrator/parser/parser.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import time
+from datetime import datetime, timedelta
+import schedule
+import os
+
+
+def get_first_level_directories(time_dif):
+    try:
+        date = (datetime.now() - timedelta(days=6-time_dif)).strftime('%Y-%m-%d')
+        day_of_year = datetime.strptime(date, '%Y-%m-%d').timetuple().tm_yday
+        directory_path = f"/home/dasha/wotiwan/archive/2024/{day_of_year}"
+        entries = os.listdir(directory_path)
+
+        directories = [entry for entry in entries if os.path.isdir(os.path.join(directory_path, entry))]
+
+        log_message("info", "200 OK Get directories.")
+        return directories
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+
+def write_directories_to_file(directories):
+    try:
+        file_path = "/home/dasha/wotiwan/orchestrator/daemons_to_load.txt"
+        with open(file_path, 'w') as file:
+            for directory in directories:
+                file.write(directory + '\n')
+    except Exception as e:
+        print(f"Error writing to file: {e}")
+
+
+def main():
+    directories = get_first_level_directories(1)
+    write_directories_to_file(directories)
+    print("Done! Going to sleep.")
+
+
+directories = get_first_level_directories(0)
+write_directories_to_file(directories)
+
+
+schedule.every(1).day.at("23:25").do(main)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
