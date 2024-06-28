6cf1c5bcb15fa7bf79d597ac114dc49c443c38e8
Posolot <138215725+Posolot@users.noreply.github.com>
2024-06-28 10:26:50 +0800

    Create log.py(logger)

---
+++ log/log.py
@@ -0,0 +1,30 @@
+import logging
+import ecs_logging
+import time
+from random import randint
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+log_file = os.path.join(current_dir, 'log.json')
+
+# Инициализация логгера
+logger = logging.getLogger("app")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(log_file)
+handler.setFormatter(ecs_logging.StdlibFormatter())
+logger.addHandler(handler)
+
+def log_message(method, message):
+    """
+    Функция для вызова метода логгера.
+
+    Args:
+        method: Метод логгера (info, warning, error, critical).
+        message: Сообщение для записи в лог.
+    """
+
+    # Получение атрибута метода логгера по имени
+    log_method = getattr(logger, method)
+
+    # Вызов метода логгера
+    log_method(message, extra={"http.request.body.content": message})
